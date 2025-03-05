13c639ff251ab511ee2c499e21eafc61604fa1f6
smart_dev <blemesh.master1030@gmail.com>
2025-03-05 02:43:10 -0500

    initial project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ckatc-app',
+  webDir: 'dist'
+};
+
+export default config;
